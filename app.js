ac149469aea80f0c9a31f291fc79f6f7a9a5a4fe
Yasidu Pathiraja <yasindupathiraja10@gmail.com>
2024-07-26 18:00:58 +0530

    customize card

---
+++ app.js
@@ -0,0 +1,20 @@
+// let angleX = 0;
+// let angleY = 0;
+
+// function setup() {
+//     createCanvas(windowWidth, windowHeight, WEBGL);
+// }
+
+// function draw() {
+//     background(2);
+
+//     rotateX(angleY);
+//     rotateY(angleX);
+
+//     box(50);
+// }
+
+// function mouseMoved() {
+//     angleX = (mouseY / height - 0.5) * TWO_PI;
+//     angleY = (mouseX / width - 0.5) * TWO_PI;
+// }
